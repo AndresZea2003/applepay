6a096d65ab9ea8f0313d4e40228e57a995f67543
Ivan Andres L <ivan.lopez@evertecinc.com>
2024-03-13 17:20:42 -0500

    wip

--- app/Domain/ApplePay/ApplePayLib/Decode/Decoder.php
+++ app/Domain/ApplePay/ApplePayLib/Decode/Decoder.php
@@ -4,6 +4,8 @@ namespace App\Domain\ApplePay\ApplePayLib\Decode;
 
 use App\Domain\ApplePay\ApplePayLib\Message\Request\DecodeRequest;
 use App\Domain\ApplePay\ApplePayLib\Message\Response\DecodeResponse;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class Decoder
 {
@@ -28,15 +30,43 @@ class Decoder
         $fileMetadata = stream_get_meta_data($file);
 
         $certificatePath =  $fileMetadata['uri'];
-
         $getCertificatesCommand = ['openssl', 'pkcs7', '-inform', 'DER', '-in', $certificatePath, '-print_certs'];
 
-        dd($fileMetadata['uri']);
-        dd(exec("openssl pkcs7 -inform DER -in $certificatePath -print_certs"));
+        try {
+            $process = new Process($getCertificatesCommand);
+            $process->mustRun();
+
+            $commandOutput = $process->getOutput();
+        } catch (ProcessFailedException $e) {
+            throw new \RuntimeException("Can't get certificates", 0, $e);
+        }
+
+        $certificates = str_replace("\n\n", "\n", rtrim($commandOutput));
+        $certificates = str_replace("-----END CERTIFICATE-----\n", "-----END CERTIFICATE-----\n\n", $certificates);
+
+
+
+        dd($certificates);
+       // return $this->normalisePrintCerts(rtrim($commandOutput));
+
+
         return DecodeResponse::fromArray([
             'token' => $request->toArray()
         ]);
 
+    }
+
+    /**
+     * @param array $command
+     * @return string
+     * @throws ProcessFailedException
+     */
+    private function runCommand(array $command)
+    {
+        $process = new Process($command);
+        $process->mustRun();
+
+        return $process->getOutput();
     }
 
 }
