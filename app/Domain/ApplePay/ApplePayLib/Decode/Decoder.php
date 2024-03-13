6a3add30bb05ff6086f77f4076d3ae776f5728fd
Ivan Andres L <ivan.lopez@evertecinc.com>
2024-03-13 17:11:13 -0500

    wip

--- app/Domain/ApplePay/ApplePayLib/Decode/Decoder.php
+++ app/Domain/ApplePay/ApplePayLib/Decode/Decoder.php
@@ -10,6 +10,8 @@ class Decoder
     public static function make(DecodeRequest $request): DecodeResponse
     {
         // TODO decode...
+
+        //obtener llave privada -> ecckey.key -> leer y obtener el contenido
  /*       Get AppleRootCA-G3.pem:
 
         Download AppleRootCA-G3.cer
@@ -21,11 +23,16 @@ class Decoder
             throw new \RuntimeException('Signature is not a valid base64 value');
         }
 
-        $certificatePath = storage_path('app/certificados/AppleRootCA-G3.pem');
+        $file = tmpfile();
+        fwrite($file, $signature);
+        $fileMetadata = stream_get_meta_data($file);
+
+        $certificatePath =  $fileMetadata['uri'];
 
         $getCertificatesCommand = ['openssl', 'pkcs7', '-inform', 'DER', '-in', $certificatePath, '-print_certs'];
 
-
+        dd($fileMetadata['uri']);
+        dd(exec("openssl pkcs7 -inform DER -in $certificatePath -print_certs"));
         return DecodeResponse::fromArray([
             'token' => $request->toArray()
         ]);
